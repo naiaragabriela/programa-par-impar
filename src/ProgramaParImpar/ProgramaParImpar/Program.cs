cc6b5244159f9e06ee36ae73b0b1cd6fe5f43238
naiaragabriela <naigabi31@gmail.com>
2023-03-05 23:34:26 -0300

    par ou impar do numero usando função

---
+++ src/ProgramaParImpar/ProgramaParImpar/Program.cs
@@ -0,0 +1,21 @@
+﻿int numero;
+
+int Numero()
+{
+    Console.WriteLine("Escreva um número: ");
+    return int.Parse(Console.ReadLine());
+}
+
+numero = Numero();
+
+void ParImpar()
+{
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("O número digitado é par");
+    }else
+    {
+        Console.WriteLine("O número digitado é ímpar");
+    }
+}
+ParImpar();
